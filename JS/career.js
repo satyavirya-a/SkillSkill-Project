3549fcff708d704b21aa1ce7b2eb018cf14ab7d8
Satya Virya Atmadja <satyaatmadja07@gmail.com>
2026-03-17 07:52:32 +0700

    Enhance About Us and Career pages with new sections, styles, and JavaScript functionality

---
+++ JS/career.js
@@ -0,0 +1,16 @@
+
+const items = document.querySelectorAll(".qna-box button");
+
+function toggleQna() {
+    const itemToggle = this.getAttribute("aria-expanded");
+
+    for (i = 0; i < items.length; i++) {
+        items[i].setAttribute("aria-expanded", "false");
+    }
+
+    if (itemToggle == "false") {
+        this.setAttribute("aria-expanded", "true");
+    }
+}
+
+items.forEach(item => item.addEventListener("click", toggleQna));
